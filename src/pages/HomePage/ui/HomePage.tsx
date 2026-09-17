8af044dfb06415ad6cdd570646e7002838f8dca7
endlesscod1ng <endlesscoding@yahoo.com>
2026-09-17 22:48:28 +0300

    cr w/Stckl

--- src/pages/HomePage/ui/HomePage.tsx
+++ src/pages/HomePage/ui/HomePage.tsx
@@ -1,11 +1,12 @@
 import { Banner } from "@/widgets/Banner/Banner";
 import s from "./HomePage.module.scss";
-import { Services } from "@/widgets/Services/Services";
+
 import { Price } from "@/widgets/Price/Price";
 import { Reviews } from "@/widgets/Reviews/Reviews";
 import { Projects } from "@/widgets/Projects/Projects";
 import { Contacts } from "@/widgets/Contacts";
 import { Page } from "@/shared/layouts/Page/Page";
+import { Stack } from "@/widgets/Stack/Stack";
 
 interface HomePageProps {
   className?: string;
@@ -18,11 +19,11 @@ export const HomePage = ({ className }: HomePageProps) => {
       className={[s.homePage, className].filter(Boolean).join(" ")}
     >
       <Banner />
+      <Stack />
       <Projects />
-      <Services />
-      <Price />
-      <Reviews />
-      <Contacts />
+      {/* <Price /> */}
+      {/* <Reviews /> */}
+      {/* <Contacts /> */}
     </Page>
   );
 };
---
+++ src/widgets/Stack/Stack.tsx
@@ -0,0 +1,50 @@
+import { AppTitle } from "@/shared/ui/AppTitle/AppTitle";
+import s from "./Stack.module.scss";
+import { Container } from "@/shared/ui/Container/Container";
+import { AppText } from "@/shared/ui/AppText/AppText";
+import CssIcon from "@/shared/assets/img/stack/css.svg?react";
+import GitIcon from "@/shared/assets/img/stack/git.svg?react";
+import HtmlIcon from "@/shared/assets/img/stack/html.svg?react";
+import JsIcon from "@/shared/assets/img/stack/js.svg?react";
+import NodejsIcon from "@/shared/assets/img/stack/nodejs.svg?react";
+import ReactIcon from "@/shared/assets/img/stack/react.svg?react";
+import ReduxIcon from "@/shared/assets/img/stack/redux.svg?react";
+import SassIcon from "@/shared/assets/img/stack/sass.svg?react";
+import TailwindIcon from "@/shared/assets/img/stack/tailwind.svg?react";
+import TypescriptIcon from "@/shared/assets/img/stack/typescript.svg?react";
+import VscodeIcon from "@/shared/assets/img/stack/vscode.svg?react";
+import VueIcon from "@/shared/assets/img/stack/vue.svg?react";
+
+interface StackProps {
+  className?: string;
+}
+const Icons = [
+  CssIcon,
+  GitIcon,
+  HtmlIcon,
+  JsIcon,
+  NodejsIcon,
+  ReactIcon,
+  ReduxIcon,
+  SassIcon,
+  TailwindIcon,
+  TypescriptIcon,
+  VscodeIcon,
+  VueIcon,
+];
+export const Stack = ({ className }: StackProps) => {
+  return (
+    <Container className={[s.stack, className].filter(Boolean).join(" ")}>
+      <AppTitle>{"My Tech Stack"}</AppTitle>
+      <AppText fontSize="l">
+        {"Technologies I’ve been working with recently"}
+      </AppText>
+      {Icons.map((Icon, i) => (
+        <Icon
+          className={`${s.icon}`}
+          key={i}
+        />
+      ))}
+    </Container>
+  );
+};
