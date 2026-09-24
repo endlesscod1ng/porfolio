e2d65f6b9987df568dba31866ed90ead2de05cf5
endlesscod1ng <endlesscoding@yahoo.com>
2026-09-24 23:00:00 +0300

    cr f/Stp

--- src/main.tsx
+++ src/main.tsx
@@ -4,10 +4,12 @@ import { BrowserRouter } from "react-router";
 
 import "./app/styles/index.scss";
 import App from "./app/App";
+import { ScrollToTop } from "./feature/ScrollTop/ScrollTop";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <BrowserRouter>
+      <ScrollToTop />
       <App />
     </BrowserRouter>
   </StrictMode>,
